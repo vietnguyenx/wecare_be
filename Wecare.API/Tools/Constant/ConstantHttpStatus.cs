c43cca696a731bf586959e621b4e802eae90d213
Nguyen Xuan Viet <imnviet@gmail.com>
2024-09-17 09:58:31 +0700

    test

--- Wecare.API/Tools/Constant/ConstantHttpStatus.cs
+++ Wecare.API/Tools/Constant/ConstantHttpStatus.cs
@@ -6,4 +6,12 @@
         public const int NO_CONTENT = 204;
         public const int NOT_FOUND = 404;
     }
+
+    public static class Constants
+    {
+        public const int TEST = 200;
+        public const int TEST_GITIGNORE = 999;
+    }
 }
+
+
