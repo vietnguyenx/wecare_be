15aef71bd03edf06e3581d61cd6959411960ab64
Viet Nguyen Xuan <125999298+vietnguyenx@users.noreply.github.com>
2024-09-23 06:25:31 +0700

    update user, healthMetric, menuDietPlan (#4)

--- Wecare.Repositories/Data/Entities/Enum/DiseaseType.cs
+++ Wecare.Repositories/Data/Entities/Enum/DiseaseType.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Wecare.Repositories.Data.Entities.Enum
 {
+<<<<<<<< HEAD:Wecare.Repositories/Data/Entities/Enum/DiseaseType.cs
     public enum DiseaseType
+========
+    public class MenuDietPlanService
+>>>>>>>> c4ed5a4 (update user, healthMetric, menuDietPlan (#4)):Wecare.Services/Services/Services/MenuDietPlanService.cs
     {
         Diabetes,
         Gout,
--- Wecare.Repositories/Data/Entities/Enum/UserType.cs
+++ Wecare.Repositories/Data/Entities/Enum/UserType.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Wecare.Repositories.Data.Entities.Enum
 {
+<<<<<<<< HEAD:Wecare.Repositories/Data/Entities/Enum/UserType.cs
     public enum UserType
+========
+    public interface IMenuDietPlanService
+>>>>>>>> c4ed5a4 (update user, healthMetric, menuDietPlan (#4)):Wecare.Services/Services/Interface/IMenuDietPlanService.cs
     {
         Free,
         Premium
